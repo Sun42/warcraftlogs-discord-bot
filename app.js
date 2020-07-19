6b9ec52d6ef288ce2395d0658a9bed9375d21b45
Christophe Sundas <sundas.christophe@gmail.com>
2020-07-19 18:49:31 +0200

    better command parsing

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ client.once('ready', () => {
 });
 
 client.on('message', (message) => {
+    /*
     console.log(message.content);
     if (message.content.startsWith(`${prefix}info`)) {
         message.channel.send("Your discord guild name: " + message.guild.name);
@@ -17,6 +18,21 @@ client.on('message', (message) => {
         message.channel.send("Your discord user name: " + message.author.username);
         message.channel.send("Your discord user id: " + message.author.id);
     }
+    */
+   if (!message.content.startsWith(prefix) || message.author.bot) {
+       console.log('Invalid message' + message.content);
+       return;
+   }
+   const args = message.content.slice(prefix.length).trim().split('/ +/');
+   const command = args.shift().toLowerCase();
+
+   if (command === 'watch') {
+       if (!args.length) {
+           error_message = 'Provide a valid url argument ex: /watch https://classic.warcraftlogs.com/guild/eu/auberdine/sigma or https://classic.warcraftlogs.com/guild/id/475922';
+           return message.channel.send(error_message);
+       }
+       const url = args.shift();
+   }
  });
 
 client.login(TOKEN);
